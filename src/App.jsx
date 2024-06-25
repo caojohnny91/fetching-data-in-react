7c3ea5ec094ec36ff63a180c20eb401bba1b4a75
jcao91 <jcao91@yahoo.com>
2024-06-25 09:16:53 -0500

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
+import * as weatherService from './services/weatherService'
+// This syntax imports everything/all (*) exported from the weatherService.js module and groups them inside a weatherService object alias. Whenever we require a specific service function, we can access it through dot notation on the weatherService object.
+
 
 function App() {
+  const fetchData = async () =>{
+    const data = await weatherService.show('New York')
+    console.log('Data: ', data);
+  }
+
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Weather API</h1>
+      <button onClick={fetchData}>Fetch Weather Data</button>
+      {/* Here, we are calling upon fetchData with an onClick event handler, and logging the data returned from ` weatherService.show(‘New York’)` to the console. */}
     </>
   );
 }
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,2 +1,27 @@
 const API_KEY = "3a86b6189ca74af2adf132756242506";
 const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
+// this is just for learning, otherwise never expose your api keys
+
+const show = async (city) => {
+  try {
+    const queryString = `&q=${city}`;
+    const res = await fetch(BASE_URL + queryString);
+    console.log("response: ", res);
+    const data = await res.json();
+    console.log("Data: ", data);
+    return data;
+  } catch (err) {
+    console.log(err);
+    console.log(
+      "There was an issue with the api request to weatherapi from the show function"
+    );
+  }
+  //   In the code block above, we are constructing a request URL, sending the request, and then parsing the response as JSON. Both fetch() and the .json() method are asynchronous, so we include the await keyword to pause the execution of the code. Notice how we define the queryString variable on its own line, and then add it to the end of the BASE_URL. This just makes our code a bit more legible.
+
+  // 💡 The .json() method converts the JSON formatted string from the response into a JavaScript object.
+};
+
+// show('New York');
+// in terminal type: node src/services/weatherService.js and this will return the weather information for New York to test
+
+export { show };
