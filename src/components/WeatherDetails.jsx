229b73f5475e85a93d0bfb5dcd909f4e35295bab
jcao91 <jcao91@yahoo.com>
2024-06-25 10:49:10 -0500

    david use effect extra lesson

--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -1,7 +1,21 @@
+import { useState, useEffect } from "react";
 const WeatherDetails = (props) => {
-    
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    console.log("Use effect with no array");
+  });
+  useEffect(() => {
+    console.log("Use effect with empty array");
+  }, []);
+  useEffect(() => {
+    console.log("Use effect with dependency");
+  }, [counter]);
+  //   all the use effects are for extra lesson from david
+
   return (
     <section>
+      <button onClick={() => setCounter(counter + 1)}>Click</button>
+      <span>counter: {counter}</span>
       <h2>Weather Details</h2>
       <p>Locations: {props.weather.location}</p>
       <p>Temperature: {props.weather.temperature}</p>
